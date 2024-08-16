bf53383bad3d064e1fdeb19e60fd0912acd807a3
Santy-Mendez <juansantiagosalvaymendez@gmail.com>
2024-08-15 21:59:48 -0300

    Actualizacion 15/8

--- Peaje/static/js/routeStatus.js
+++ Peaje/static/js/routeStatus.js
@@ -2,16 +2,17 @@ function setProgress(percent, element) {
     const circle = element.querySelector('.progress-ring__circle');
     const radius = circle.r.baseVal.value;
     const circumference = 2 * Math.PI * radius;
-  
+
+   
     circle.style.strokeDasharray = `${circumference} ${circumference}`;
     circle.style.strokeDashoffset = `${circumference}`;
-  
+
     const offset = circumference - (percent / 100) * circumference;
-    
-    // Velocidad de la animación en ms (más bajo = más rápido)
-    const animationSpeed = 7;
-  
-    // Animación del círculo
+
+
+    const animationSpeed = 30; 
+
+
     let currentOffset = circumference;
     const interval = setInterval(() => {
         if (currentOffset <= offset) {
@@ -21,24 +22,24 @@ function setProgress(percent, element) {
             circle.style.strokeDashoffset = currentOffset;
         }
     }, animationSpeed);
-  
-    // Animación del porcentaje
+
+
     const percentageText = element.querySelector('.percentage');
     let currentPercent = 0;
     const percentageInterval = setInterval(() => {
-      if (currentPercent >= percent) {
-        clearInterval(percentageInterval);
-      } else {
-        currentPercent++;
-        percentageText.textContent = `${currentPercent}%`;
-      }
-    }, 700 / percent); // Este valor controla la velocidad del texto
+        if (currentPercent >= percent) {
+            clearInterval(percentageInterval);
+        } else {
+            currentPercent++;
+            percentageText.textContent = `${currentPercent}%`;
+        }
+    }, 3100 / percent); 
 }
-  
-// Aplicar la animación al cargar la página
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const progressCircles = document.querySelectorAll('.progress-circle');
     progressCircles.forEach(circle => {
-        setProgress(100, circle); // Cambia 100 por el porcentaje que desees animar
+        setProgress(100, circle); 
     });
 });
