f2ecb053b607244df910191bc650c8216b07396d
Santy-Mendez <juansantiagosalvaymendez@gmail.com>
2024-08-26 11:43:08 -0300

    Actualizaciones 26/8

--- Peaje/actividad/urls.py
+++ Peaje/actividad/urls.py
@@ -3,16 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('telepase/', views.telepase, name='telepase'),
-    path('quienes-somos/', views.quienes_somos, name='quienes_somos'),
-    path('servicios/', views.servicios, name='servicios'),
-    path('atencion-al-cliente/', views.atencion_al_cliente, name='atencion_al_cliente'),
-    path('login/', views.login, name='login'),
-    path('registro/', views.registro, name='registro'),
-    path('recuperar-contrasena/', views.recuperar_contrasena, name='recuperar_contrasena'),
-    path('obras/', views.obras, name='obras'),
-    path('foro/', views.foro, name='foro'),
-    path('obras/obras-ejecutadas', views.obras_ejecutadas, name='obras1'),
-    path('obras/obras-en-ejecucion', views.obras_ejecucion, name='obras2')
-
 ]
--- Peaje/actividad/views.py
+++ Peaje/actividad/views.py
@@ -2,36 +2,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
-
-def telepase(request):
-    return render(request, 'telepase.html')
-
-def quienes_somos(request):
-    return render(request, 'QUIENES-SOMOS.html')
-
-def servicios(request):
-    return render(request, 'SERVICIOS.html')
-
-def atencion_al_cliente(request):
-    return render(request, 'ATENCION-AL-CLIENTE.html')
-
-def login(request): 
-    return render(request, 'LOGIN.html')
-
-def registro(request):
-    return render(request, 'REGISTER.html')
-
-def recuperar_contrasena(request):
-    return render(request, 'RECUCONTRA.html')
-
-def foro(request):
-    return render(request, 'Foro.html')
-
-def obras(request):
-    return render(request, 'Obras.html')
-
-def obras_ejecucion(request):
-    return render(request, 'obras_e.html')
-
-def obras_ejecutadas(request):
-    return render(request, 'obras_ec.html')
