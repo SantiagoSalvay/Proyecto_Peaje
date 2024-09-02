7b7cf5ca55c72368305c6f4b7335e290345c54d6
Santy-Mendez <juansantiagosalvaymendez@gmail.com>
2024-09-02 00:23:33 -0300

    Actualizacion 2/9

--- Peaje/actividad/urls.py
+++ Peaje/actividad/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.cobro, name='index'),
     path('casilla/', views.Casilla, name='casilla'),
     path('casilla/comienza-turno/', views.comienza_turno, name='comienza_turno'),
+    path('casilla/comienza-turno/cobro', views.cobro, name='cobro'),
+    path('generar-factura/', views.generar_factura, name='generar_factura'),
+
 
 ]
--- Peaje/static/js/casilla.js
+++ Peaje/static/js/casilla.js
@@ -5,10 +5,9 @@ document.addEventListener("DOMContentLoaded", function() {
     casillas.forEach((casilla, index) => {
         casilla.querySelector('.numero').textContent = index + 1;
 
-        // Agregar evento de clic para redirigir a la página de detalle de casilla
+
         casilla.addEventListener('click', function() {
             const numeroCasilla = index + 1;
-            // Redirigir a la nueva página con el número de casilla en la URL
             window.location.href = `${comienzaTurnoUrl}?casilla=${numeroCasilla}`;
         });
     });
