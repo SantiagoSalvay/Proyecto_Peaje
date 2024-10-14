43ee32a9cc2422d18bfd6a46d7ac928c622c19b9
Santy-Mendez <juansantiagosalvaymendez@gmail.com>
2024-10-14 11:02:38 -0300

    Actualizacion  14/10

--- Peaje_Proyecto/settings.py
+++ Peaje_Proyecto/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Peaje_Proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'peaje_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOTS': 'localhost',
+        'PORT': '3306',
     }
 }
 
